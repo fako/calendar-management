9b41ddc898bf7172681b0601f98b63509a804773
Fako Berkers <email@fakoberkers.nl>
2020-01-02 19:51:03 +0100

    Generate PDF based on HTML page

--- future_fashion/tasks.py
+++ future_fashion/tasks.py
@@ -2,6 +2,8 @@ import os
 from invoke import task
 from math import floor
 
+from pandas.io.formats.format import Timedelta64Formatter
+
 from calendar_service import (get_calendar_service, get_time_boundries, get_confirmed_nonoverlap_events_frame,
                               get_template_engine)
 
@@ -22,16 +24,39 @@ def report_events(ctx, username, year, start_month=1, end_month=12, report=False
     frame = get_confirmed_nonoverlap_events_frame(events)
     if frame is None:
         return
-    # Create report
-    title = f"Uren registratie {calendar_id} voor {year}-{start_month} t/m {year}-{end_month}"
+    # Get basic aggregations
     total_time = frame["duration"].sum().total_seconds()
     total_hours = total_time / 60 / 60
-    engine = get_template_engine()
+    residu_hours = total_hours % 8
+    # Write report
     if report:
-        report_file = os.path.join("future_fashion", "reports", f"{year}-{start_month} tm {year}-{end_month}.html")
+        # Get template engine and basic variables
+        engine = get_template_engine()
+        if start_month != end_month:
+            file_name = f"{username}_{year}-{start_month}_tm_{year}-{end_month}"
+            title = f"Uren registratie {calendar_id} voor {year}-{start_month} t/m {year}-{end_month}"
+        else:
+            file_name = f"{username}_{year}-{start_month}"
+            title = f"Uren registratie {calendar_id} voor {year}-{start_month}"
+        # Format time delta's without seconds
+        frame["duration"] = Timedelta64Formatter(frame["duration"]).get_result()
+        frame["duration"] = frame["duration"].apply(lambda dur: dur[:-3])
+        # Translate the output
+        frame = frame.rename(axis="columns", mapper={
+            "activity": "activiteit",
+            "day": "dag",
+            "end": "einde",
+            "duration": "duur (uren:minuten)"
+        })
+        # Write to HTML file
+        report_file = os.path.join("future_fashion", "reports", f"{file_name}.html")
         report = engine.get_template("report.html")
         with open(report_file, "w") as fd:
-            fd.write(report.render(title=title, table=frame.to_html(), total_hours=total_hours))
+            fd.write(report.render(title=title, table=frame.to_html(index=False), total_hours=total_hours,
+                                   residu_hours=residu_hours))
+        # Create PDF
+        with ctx.cd(os.path.join("future_fashion", "reports")):
+            ctx.run(f"wkhtmltopdf {file_name}.html {file_name}.pdf")
     # CLI output
     print(f"Total hours: {total_hours}")
-    print(f"Total time: {floor(total_hours/8)} days and {total_hours%8} hours")
+    print(f"Total time: {floor(total_hours/8)} days and {residu_hours} hours")
